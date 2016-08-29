b5b49d232235860f7b31a58ae4c6bf8bc732397c
swapnilP <swapnil.p@amuratech.com>
2016-08-29 14:46:15 +0530

    Final

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,7 +4,7 @@ class Employee < ActiveRecord::Base
   validates :name, format: { with: /\A[a-zA-Z ]+\z/,message: "Alphabets and spaces" }
   validates :phone, uniqueness:true , format: { with: /\d{10}/,message: "Invalid Phone Format,must be 10 digits"}
   validates :email, uniqueness: true, format: {with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,message:"Email Format Invalid"}
-  validates_numericality_of :min_age, greater_than: 0
+  validates_numericality_of :salary, greater_than: 0
 
   #associations
   belongs_to :company
