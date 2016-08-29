d30061a960e4bb23dfd8f67ce67719f471d412dd
swapnilP <swapnil.p@amuratech.com>
2016-08-29 10:02:23 +0530

    addresses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827201227) do
+ActiveRecord::Schema.define(version: 20160829043027) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string   "street"
+    t.string   "city"
+    t.integer  "pincode"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "addresses", ["resource_type", "resource_id"], name: "index_addresses_on_resource_type_and_resource_id"
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
