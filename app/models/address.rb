8d054aa021eeef7bc158cb8e353ab45152070144
swapnilP <swapnil.p@amuratech.com>
2016-08-29 18:07:42 +0530

    Queries to Class methods on Models added

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ActiveRecord::Base
   belongs_to :resource, polymorphic: true
+
+  def self.find_employees_by_city(city)
+    Address.where(:city=>city,:resource_type=>"Employee").map(&:resource)
+  end
+
+  def self.find_employees_by_city_2(city)
+    Employee.includes(:address).where(addresses:{city:city})
+  end
+  
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -9,4 +9,22 @@ class Employee < ActiveRecord::Base
   #associations
   belongs_to :company
   has_one :address,as: :resource
+
+  def self.top_five()
+    Employee.all.select('id,name,salary').order(salary: :desc).limit(5)
+  end
+
+  def self.find_by_salary_range(a=10000,b=50000)
+    Employee.select('id,name,salary').where(:salary=>(a..b))
+  end
+
+  def self.search_by_name(name)
+    Employee.joins(:address).where(:name=>name).pluck("name,email,street,city,pincode")
+  end
+
+  def self.search_by_address(email)
+    Employee.joins(:address).where(:email=>email).pluck("name,email,street,city,pincode")
+  end
+
+
 end
