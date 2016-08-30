04b0c650155e201bb58f5a539c781768deb9f677
swapnilP <swapnil.p@amuratech.com>
2016-08-30 18:08:05 +0530

    company with show new actions

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,6 +4,28 @@ class CompaniesController < ApplicationController
       @companies=Company.includes(:address).all
     end
 
-    
+    def new
+      @company=Company.new
+    end
 
+    def create
+      @c=Company.new(company_params)
+      @c.build_address(address_params)
+
+      if @c.save
+        redirect_to company_path
+      else
+        render 'new'
+      end
+
+    end
+
+    private
+        def company_params
+            params.require(:company).permit(:name)
+        end
+
+        def address_params
+            params.require(:company).require(:address).permit(:street,:city,:pincode)
+        end
 end
