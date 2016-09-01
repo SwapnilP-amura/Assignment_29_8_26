eb2e0f634879d47e399042a911e0f812218d89f6
swapnilP <swapnil.p@amuratech.com>
2016-09-01 12:09:07 +0530

    All actions complete

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,51 +1,49 @@
 class EmployeesController < ApplicationController
 
-  #before_action :get_employee ,only: [:show,:edit,:destroy,:update]
-
+  before_action :get_employee ,only: [:show,:edit,:destroy,:update]
+  before_action :get_company ,only: [:index,:new,:create]
   def index
-    #render plain: params
-     @company=Company.find(params[:company_id])
-     #render plain: @company.employees.inspect
-    @employees=@company.employees
+     #@company=Company.find(params[:company_id])
+     @employees=@company.employees
   end
 
   def new
-    @employee=Employee.new
+    #@company=Company.find(params[:company_id])
   end
 
   def show
-      @employee=Employee.find(params[:id])
+      #@employee=Employee.find(params[:id])
   end
 
   def edit
-      @employee=Employee.find(params[:id])
-      #render plain: params
+      #@employee=Employee.find(params[:id])
   end
 
   def create
-    @employee=Employee.new(employee_params)
-    @employee.build_address(address_params)
+      @company=Company.find(params[:company_id])
+      @employee=@company.employees.new(employee_params)
+      @employee.build_address(address_params)
 
-    if @employee.save
-      redirect_to employees_path
-    else
-      render 'new'
-    end
+     if @employee.save
+        redirect_to company_employees_path(@company)
+      else
+        redirect_to new_company_employee_path(@company)
+      end
   end
 
   def update
-    @employee = Employee.find(params[:id])
+      #@employee = Employee.find(params[:id])
       if @employee.update(employee_params) and @employee.address.update(address_params)
-        redirect_to employee_path
+        redirect_to company_employees_path(@employee.company)
       else
         render 'edit'
       end
   end
 
   def destroy
-      @employee=Employee.find(params[:id])
+      #@employee=Employee.find(params[:id])
       @employee.destroy
-      redirect_to employees_path
+      redirect_to company_employees_path(@employee.company)
   end
 
   private
@@ -56,7 +54,12 @@ class EmployeesController < ApplicationController
     def address_params
       params.require(:employee).require(:address).permit(:street,:city,:pincode)
     end
-  #   def get_employee
-  #     @employee=Employee.find(params[:id])
-  #  end
+
+    def get_employee
+       @employee=Employee.find(params[:id])
+    end
+
+    def get_company
+      @company=Company.find(params[:company_id])
+    end
 end
